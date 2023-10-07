ee08b7268640c8d289fcd9bff099b142b48247ad
Aaryan Bhagat <bhagataaryan2004@gmail.com>
2023-10-07 23:30:06 +0530

    Commit

--- project-1/src/App.tsx
+++ project-1/src/App.tsx
@@ -1,8 +1,21 @@
 // import Message from "./Message";
-import ListGroup from "./components/Listgroup";
+// import ListGroup from "./components/Listgroup";
 
-function App(){
-  return <div><ListGroup /></div>
+// function App(){
+//   return <div><ListGroup /></div>
+// }
+
+// export default App;
+
+function MyButton(){
+  return <button>This is a button</button>
 }
 
-export default App;
+export default function App(){
+  return (
+  <div>
+    <h1>This is the main App Function</h1>
+    <MyButton></MyButton>
+  </div>
+  );
+}
